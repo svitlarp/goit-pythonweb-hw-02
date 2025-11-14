8d14996bec540e23a40aacfd99443f3d0a904498
svitlarp <svitlanka.ripa@gmail.com>
2025-11-14 23:29:36 +0100

    rebuild docker-compose and db.py in way that app also use secrets.

--- conf/db.py
+++ conf/db.py
@@ -2,12 +2,24 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
 
-DB_USER= os.getenv("POSTGRES_USER")
-DB_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-DB_HOST = os.getenv("POSTGRES_HOST")
-DB_PORT = os.getenv("POSTGRES_PORT")
+#  read db_name from secrets
+dbname_file = os.getenv("POSTGRES_DB_FILE")
+with open(dbname_file, "r") as f:
+    DB_NAME = f.read().strip()
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/hw02"
+#  read username from secrets
+user_file = os.getenv("POSTGRES_USER_FILE")
+with open(user_file, "r") as f:
+    DB_USER = f.read().strip()
+
+#  read password from secrets
+passwd_file = os.getenv("POSTGRES_PASSWORD_FILE")
+with open(passwd_file, "r") as f:
+    DB_PASSWORD = f.read().strip()
+
+
+SQLALCHEMY_DATABASE_URL = (f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@postgres:5432/{DB_NAME}")
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
