0a9bd9b2276dec04680c297da779fb273f09970a
svitlarp <svitlanka.ripa@gmail.com>
2025-11-15 00:17:09 +0100

    revome secrets, sensetive info are exposed , result -> "Welcome to FastAPI!"

--- conf/db.py
+++ conf/db.py
@@ -2,24 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 import os
 
-#  read db_name from secrets
-dbname_file = os.getenv("POSTGRES_DB_FILE")
-with open(dbname_file, "r") as f:
-    DB_NAME = f.read().strip()
-
-#  read username from secrets
-user_file = os.getenv("POSTGRES_USER_FILE")
-with open(user_file, "r") as f:
-    DB_USER = f.read().strip()
-
-#  read password from secrets
-passwd_file = os.getenv("POSTGRES_PASSWORD_FILE")
-with open(passwd_file, "r") as f:
-    DB_PASSWORD = f.read().strip()
-
-
-SQLALCHEMY_DATABASE_URL = (f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@postgres:5432/{DB_NAME}")
-
+# SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:567234@postgres:5432/hw02"
+SQLALCHEMY_DATABASE_URL = os.getenv("DB_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -36,3 +20,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
